4264b327658c8c5211d87612b44472b830d9e8b2
robedouard <robedouard@gmail.com>
2017-02-01 15:28:39 -0500

    renaming to multiply

--- fmtPrintln_test.go
+++ fmtPrintln_test.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 )
 
-func main(){
+func main() {
 	fmt.Println(len("Hello World"))
 	fmt.Println("Hello World"[1])
 	fmt.Println("Hello " + "World")
-	fmt.Println(321325 * 424521)
+	fmt.Println(3 * 4)
 	fmt.Println((true && false) || (false && true) || !(false && false))
 }
