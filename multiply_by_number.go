889c2e0f4f98aaf049227013fa3c6c2c81d33205
robedouard <robedouard@gmail.com>
2018-05-26 19:59:13 -0400

    minor change

--- multiply_by_number.go
+++ multiply_by_number.go
@@ -8,6 +8,6 @@ func main() {
 	fmt.Println("Enter a number. We will multiply it by two:")
 	var theNumber float64
 	fmt.Scanf("%f", &theNumber)
-	output := theNumber * 2
+	output := theNumber * 4
 	fmt.Println(output)
 }
