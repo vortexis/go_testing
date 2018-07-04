2500d274a16744bea37abf47d089539eec93f265
robedouard <robedouard@gmail.com>
2018-07-04 00:15:02 -0400

    adding find_large

---
+++ find_large.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+var loopcontrol, b, c int
+
+func main() {
+
+	x := []int{55, 20, 88, 100, 555, 33, 3, 95, 33, 84, 21, 93, 36, 9, 54, 82, 4}
+
+	fmt.Println("This array has the following numbers: ", x[:])
+
+	b := x[0]
+
+	for loopcontrol = 0; loopcontrol < len(x); loopcontrol++ {
+
+		fmt.Println(loopcontrol, "we are evaluating: ", x[loopcontrol], "against", b)
+
+		if b >= x[loopcontrol] {
+
+			b = b
+
+		} else {
+
+			b = x[loopcontrol]
+
+		}
+
+		c = b
+
+	}
+
+	fmt.Println("\nThe highest number in this Array is: ", c)
+
+}
