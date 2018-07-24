a5256a984a2c087d2c2ad1571bc8605c32e00c05
robedouard <robedouard@gmail.com>
2018-07-24 01:29:58 -0400

    small mods

--- write_file.go
+++ write_file.go
@@ -19,12 +19,4 @@ func main() {
 
 	}
 
-	fmt.Println("Hello please enter a value:\n")
-
-	var userInput string
-
-	fmt.Scanf("%s", &userInput)
-
-	fmt.Println("This is your input:\n", userInput)
-
 }
