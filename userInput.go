1eabdd45bc0dfe092d3377dc9942a1691f802284
Robert Edouard <robedouard@gmail.com>
2019-08-24 21:11:39 -0400

    adding hidden user input

--- userInput.go
+++ userInput.go
@@ -2,13 +2,19 @@ package main
 
 import "fmt"
 
+// This import is for the hidden user input
+import "golang.org/x/crypto/ssh/terminal"
+
 func main() {
 
-	fmt.Println("Hello please enter a value:\n")
-
 	var userInput string
-
+	fmt.Println("Hello please enter a value :\n")
 	fmt.Scanf("%s", &userInput)
+	fmt.Println("Here is your input from scanf :", userInput)
 
-	fmt.Println("This is your input:\n", userInput)
+	// Hide user input
+	fmt.Println("Enter hidden input :\n")
+	hiddenPass, _ := terminal.ReadPassword(0)
+	fmt.Printf("Here is your input from terminal.ReadPassword : %s", hiddenPass)
+
 }
